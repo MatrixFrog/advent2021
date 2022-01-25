5938599d922a6382a2056e5ff636810d61f7bca3
Tyler Breisacher <tbreisacher@drivers.coop>
2022-01-24 22:12:48 -0800

    reworked level 6 for part 2

--- rs/src/bin/level6/main.rs
+++ rs/src/bin/level6/main.rs
@@ -1,48 +1,41 @@
-#[derive(Debug, Hash, Eq, PartialEq, Copy, Clone)]
-struct Fish {
-  value: u8,
-}
+use std::collections::hash_map::HashMap;
 
 fn raw_input() -> &'static str {
   include_str!("input.txt")
   // "3,4,3,1,2"
 }
 
-fn input() -> Vec<Fish> {
+fn input() -> Vec<u8> {
   let mut fish = Vec::new();
   for l in raw_input().split(',') {
-    fish.push(Fish {
-      value: l.trim().parse().unwrap(),
-    })
+    fish.push(l.trim().parse().unwrap());
   }
   fish
 }
 
-fn tick(fish: &mut Vec<Fish>) {
-  let mut new_fish = 0;
-  for f in fish.iter_mut() {
-    if f.value == 0 {
-      f.value = 6;
-      new_fish += 1;
-    } else {
-      f.value -= 1;
-    }
+fn input_as_map() -> HashMap<u8, u64> {
+  let mut aquarium: HashMap<u8, u64> = HashMap::new();
+  for f in input() {
+    *aquarium.entry(f).or_default() += 1;
   }
-  for _ in 0..new_fish {
-    fish.push(Fish { value: 8 });
-  }
+  aquarium
 }
 
-fn solve(turns: i32) -> usize {
-  let mut fish = input();
-  for _ in 0..turns {
-    tick(&mut fish)
+fn tick(aquarium: &HashMap<u8, u64>)->HashMap<u8, u64> {
+  let mut new_aquarium: HashMap<u8, u64> = HashMap::new();
+  for i in 0..9 {
+    new_aquarium.insert(i, *aquarium.get(&(i+1)).unwrap_or(&0));
   }
-
-  fish.len()
+  new_aquarium.insert(6, new_aquarium.get(&6).unwrap_or(&0) +  aquarium.get(&0).unwrap_or(&0));
+  new_aquarium.insert(8, *aquarium.get(&0).unwrap_or(&0));
+  new_aquarium
 }
 
 fn main() {
-  println!("part 1: {}", solve(80));
-  println!("part 2: {}", solve(256));
+  let turns = 256;
+  let mut aquarium = input_as_map();
+  for _ in 0..turns {
+    aquarium = tick(&aquarium);
+  }
+  println!("{:?}", aquarium.values().sum::<u64>());
 }
