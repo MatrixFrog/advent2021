5ad78b4b58a08356a0c91a5022dcd1a4672e7160
Tyler Breisacher <tbreisacher@drivers.coop>
2022-01-26 02:38:05 -0800

    get started on part 2

--- rs/src/bin/level9/main.rs
+++ rs/src/bin/level9/main.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use std::collections::HashSet;
 
 struct Floor {
   data: Vec<Vec<u32>>,
@@ -20,10 +21,14 @@ impl Floor {
     self.data.get(r).and_then(|row| row.get(c))
   }
 
-  fn low_point(&self, p: (usize, usize)) -> bool {
+  fn neighbors(&self, p: (usize, usize)) -> impl Iterator<Item = (usize, usize)> + '_ {
+    surrounding_coordinates(p).filter(|sc| self.get(*sc).is_some())
+  }
+
+  fn is_low_point(&self, p: (usize, usize)) -> bool {
     let v = self.get(p).unwrap();
-    for neighbor in surrounding_coordinates(p).map(|sc| self.get(sc)).flatten() {
-      if v < neighbor {
+    for neighbor in self.neighbors(p) {
+      if v < self.get(neighbor).unwrap() {
         // ok
       } else {
         return false;
@@ -32,8 +37,20 @@ impl Floor {
     true
   }
 
+  fn low_points(&self) -> impl Iterator<Item = (usize, usize)> + '_ {
+    self.positions().filter(|p| self.is_low_point(*p))
+  }
+
+  // Given a low point 'p', find the size of the basin for p.
+  fn basin(&self, p: (usize, usize)) -> usize {
+    let mut basin = HashSet::new();
+    basin.insert(p);
+    // TODO...
+    basin.len()
+  }
+
   fn risk_level(&self, p: (usize, usize)) -> u32 {
-    if self.low_point(p) {
+    if self.is_low_point(p) {
       self.get(p).unwrap() + 1
     } else {
       0
@@ -44,13 +61,26 @@ impl Floor {
     (0..self.data.len()).cartesian_product(0..self.data[0].len())
   }
 
-  fn solve(&self) -> u32 {
+  fn solve_part1(&self) -> u32 {
     self.positions().map(|p| self.risk_level(p)).sum()
+  }
+
+  fn solve_part2(&self) -> u32 {
+    // TODO...
+    0
   }
 }
 
+// fn raw_input() -> &'static str {
+//   include_str!("input.txt")
+// }
+
 fn raw_input() -> &'static str {
-  include_str!("input.txt")
+  "2199943210
+3987894921
+9856789892
+8767896789
+9899965678"
 }
 
 fn input() -> Floor {
@@ -68,5 +98,6 @@ fn input() -> Floor {
 
 fn main() {
   let f = input();
-  println!("{}", f.solve())
+  println!("{}", f.solve_part1());
+  println!("{}", f.solve_part2());
 }
