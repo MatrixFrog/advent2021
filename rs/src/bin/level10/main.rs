176b69d062325bb70e7d5147708474589217a964
Tyler Breisacher <tbreisacher@drivers.coop>
2022-01-26 22:14:34 -0800

    First shot at parsing

--- rs/src/bin/level10/main.rs
+++ rs/src/bin/level10/main.rs
@@ -2,4 +2,80 @@ fn raw_input() -> &'static str {
   include_str!("input.txt")
 }
 
-fn main() {}
+fn input() -> impl Iterator<Item = &'static str> {
+  raw_input().lines()
+}
+
+#[derive(Debug)]
+enum ParseResult {
+  OK, // apparently never happens
+  Incomplete,
+  Corrupt(Vec<char>),
+}
+
+struct Parser {
+  stack: Vec<char>,
+  errors: Vec<char>,
+}
+
+impl Parser {
+  fn new() -> Self {
+    Parser {
+      stack: vec![],
+      errors: vec![],
+    }
+  }
+
+  fn check(&self, ch: char) -> bool {
+    match self.stack.last() {
+      None => panic!(),
+      Some(last) => match last {
+        '(' => ch == ')',
+        '[' => ch == ']',
+        '{' => ch == '}',
+        '<' => ch == '>',
+        _ => panic!(),
+      },
+    }
+  }
+
+  fn try_pop(&mut self, ch: char) {
+    if self.check(ch) {
+      self.stack.pop();
+    } else {
+      self.errors.push(ch);
+    }
+  }
+
+  fn handle_char(&mut self, ch: char) {
+    match ch {
+      '(' | '[' | '{' | '<' => self.stack.push(ch),
+      ')' | ']' | '}' | '>' => self.try_pop(ch),
+      _ => panic!("Unexpected char {}", ch),
+    }
+  }
+
+  fn parse_line(&mut self, line: &str) -> ParseResult {
+    for ch in line.chars() {
+      self.handle_char(ch);
+    }
+    if !self.errors.is_empty() {
+      ParseResult::Corrupt(self.errors.clone())
+    } else if !self.stack.is_empty() {
+      ParseResult::Incomplete
+    } else {
+      panic!()
+    }
+  }
+}
+
+fn parse_line(line: &str) -> ParseResult {
+  let mut p = Parser::new();
+  p.parse_line(line)
+}
+
+fn main() {
+  for l in input() {
+    println!("{:?}", parse_line(l));
+  }
+}
