5e3443cfd9d9bae606b3063e2a19d2d864eeb52a
Tyler Breisacher <tbreisacher@drivers.coop>
2022-01-26 17:30:44 -0800

    small refactor part 6

--- rs/src/bin/level6/main.rs
+++ rs/src/bin/level6/main.rs
@@ -1,13 +1,8 @@
 use std::collections::hash_map::HashMap;
 
-fn raw_input() -> &'static str {
-  include_str!("input.txt")
-  // "3,4,3,1,2"
-}
-
 fn input() -> Vec<u8> {
   let mut fish = Vec::new();
-  for l in raw_input().split(',') {
+  for l in include_str!("input.txt").split(',') {
     fish.push(l.trim().parse().unwrap());
   }
   fish
@@ -21,21 +16,32 @@ fn input_as_map() -> HashMap<u8, u64> {
   aquarium
 }
 
-fn tick(aquarium: &HashMap<u8, u64>)->HashMap<u8, u64> {
+fn tick(aquarium: &HashMap<u8, u64>) -> HashMap<u8, u64> {
   let mut new_aquarium: HashMap<u8, u64> = HashMap::new();
   for i in 0..9 {
-    new_aquarium.insert(i, *aquarium.get(&(i+1)).unwrap_or(&0));
+    new_aquarium.insert(i, *aquarium.get(&(i + 1)).unwrap_or(&0));
   }
-  new_aquarium.insert(6, new_aquarium.get(&6).unwrap_or(&0) +  aquarium.get(&0).unwrap_or(&0));
+  new_aquarium.insert(
+    6,
+    new_aquarium.get(&6).unwrap_or(&0) + aquarium.get(&0).unwrap_or(&0),
+  );
   new_aquarium.insert(8, *aquarium.get(&0).unwrap_or(&0));
   new_aquarium
 }
 
-fn main() {
-  let turns = 256;
+fn solve(part: i32) -> u64 {
+  let turns = match part {
+    1 => 80,
+    2 => 256,
+    _ => panic!("no"),
+  };
   let mut aquarium = input_as_map();
   for _ in 0..turns {
     aquarium = tick(&aquarium);
   }
-  println!("{:?}", aquarium.values().sum::<u64>());
+  aquarium.values().sum::<u64>()
+}
+
+fn main() {
+  println!("1: {}\n2: {}", solve(1), solve(2));
 }
