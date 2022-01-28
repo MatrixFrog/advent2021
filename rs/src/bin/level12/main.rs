408b9ff6dfc75e7a8a66049fcc9a29f0782dc920
Tyler Breisacher <tbreisacher@drivers.coop>
2022-01-27 19:22:00 -0800

    level 12 part 2 kinda slow but it worked

--- rs/src/bin/level12/main.rs
+++ rs/src/bin/level12/main.rs
@@ -9,17 +9,23 @@ struct Cli {
 }
 
 #[derive(Debug, Eq, PartialEq)]
-enum CaveSize {
+enum CaveType {
   Big,
   Small,
+  StartOrEnd,
 }
 
-impl CaveSize {
+impl CaveType {
   fn from_label(label: &str) -> Self {
-    if label.to_ascii_lowercase() == label {
-      Self::Small
-    } else {
-      Self::Big
+    match label {
+      "start" | "end" => Self::StartOrEnd,
+      _ => {
+        if label.to_ascii_lowercase() == label {
+          Self::Small
+        } else {
+          Self::Big
+        }
+      }
     }
   }
 }
@@ -31,7 +37,7 @@ fn small_input() -> &'static str {
   include_str!("input_small.txt")
 }
 
-fn parse_nodes<'a>(input: &'a str, g: &mut UnGraph<CaveSize, ()>) -> HashMap<&'a str, NodeIndex> {
+fn parse_nodes<'a>(input: &'a str, g: &mut UnGraph<CaveType, ()>) -> HashMap<&'a str, NodeIndex> {
   let mut node_map = HashMap::new();
   for l in input.lines() {
     let nodes = l.split('-').collect::<Vec<_>>();
@@ -40,7 +46,7 @@ fn parse_nodes<'a>(input: &'a str, g: &mut UnGraph<CaveSize, ()>) -> HashMap<&'a
       if node_map.contains_key(label) {
         // Already added.
       } else {
-        let node = g.add_node(CaveSize::from_label(label));
+        let node = g.add_node(CaveType::from_label(label));
         node_map.insert(label, node);
       }
     }
@@ -56,7 +62,7 @@ fn parse_edges(input: &str) -> impl Iterator<Item = (&str, &str)> {
   })
 }
 
-fn parse_input(input: &str) -> (UnGraph<CaveSize, ()>, NodeIndex, NodeIndex) {
+fn parse_input(input: &str) -> (UnGraph<CaveType, ()>, NodeIndex, NodeIndex) {
   let mut g = Graph::new_undirected();
   let nodes = parse_nodes(input, &mut g);
   let edges = parse_edges(input);
@@ -68,26 +74,70 @@ fn parse_input(input: &str) -> (UnGraph<CaveSize, ()>, NodeIndex, NodeIndex) {
   (g, *nodes.get("start").unwrap(), *nodes.get("end").unwrap())
 }
 
+fn is_valid(g: &UnGraph<CaveType, ()>, path: &[NodeIndex]) -> bool {
+  let mut frequencies = HashMap::new();
+  for node in path {
+    *frequencies.entry(node).or_insert(0) += 1;
+  }
+
+  let mut already_found_2 = false;
+  for (node_index, freq) in frequencies {
+    let node_type = g.node_weight(*node_index).unwrap();
+    match node_type {
+      CaveType::Big => { /* no problem */ }
+      CaveType::StartOrEnd => {
+        if freq > 1 {
+          return false;
+        }
+      }
+      CaveType::Small => {
+        if freq > 2 {
+          return false;
+        }
+        if freq == 2 {
+          if already_found_2 {
+            return false;
+          }
+          already_found_2 = true;
+        }
+      }
+    }
+  }
+  true
+}
+
+// Try to extend this path by adding 'neighbor' if doing so is valid. If not, returns None.
+fn try_extend(
+  g: &UnGraph<CaveType, ()>,
+  path: &[NodeIndex],
+  neighbor: NodeIndex,
+) -> Option<Vec<NodeIndex>> {
+  let mut new_path = path.to_owned();
+  new_path.push(neighbor);
+  if is_valid(g, &new_path) {
+    Some(new_path)
+  } else {
+    None
+  }
+}
+
 // Returns a Vec of all valid paths that can be formed by
 // adding one additional step to the given path.
 //
 // If there are none (because there is no way to continue without
 // illegally double-visiting a cave), returns an empty Vec.
-fn extend_path(g: &UnGraph<CaveSize, ()>, path: &[NodeIndex]) -> Vec<Vec<NodeIndex>> {
+fn extend_path(g: &UnGraph<CaveType, ()>, path: &[NodeIndex]) -> Vec<Vec<NodeIndex>> {
   let mut paths = vec![];
   for neighbor in g.neighbors(*path.last().unwrap()) {
-    if *g.node_weight(neighbor).unwrap() == CaveSize::Small && path.contains(&neighbor) {
-      // Can't visit this neighbor again
-    } else {
-      let mut new_path = path.to_owned();
-      new_path.push(neighbor);
-      paths.push(new_path);
+    match try_extend(g, path, neighbor) {
+      None => {}
+      Some(new_path) => paths.push(new_path),
     }
   }
   paths
 }
 
-fn find_paths(g: &UnGraph<CaveSize, ()>, start: NodeIndex, end: NodeIndex) -> Vec<Vec<NodeIndex>> {
+fn find_paths(g: &UnGraph<CaveType, ()>, start: NodeIndex, end: NodeIndex) -> Vec<Vec<NodeIndex>> {
   let mut paths = vec![vec![start]];
   loop {
     let mut keep_going = false;
