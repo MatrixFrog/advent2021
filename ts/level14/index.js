740cb3ecdb9c346407d0226fe4c51b5436a6890c
Tyler Breisacher <tbreisacher@drivers.coop>
2022-01-29 19:44:37 -0800

    Getting closer to my original idea but it's not working yet

--- ts/level14/index.js
+++ ts/level14/index.js
@@ -20,25 +20,59 @@ function parseRules(ruleLines) {
   return result;
 }
 
-function* applyGen(prevState, rules) {
-  for (let i = 0; i < prevState.length - 1; i++) {
-    yield prevState[i];
-    yield rules[prevState[i] + prevState[i + 1]];
+// Mimicking .windows2()
+//   for (const pair of 'abcd') {
+//     console.log(pair); // ['a', 'b'] then ['b', 'c'] then ['c', 'd']
+//   }
+function* iterateByTwos(iterable) {
+  let two = [];
+  for (const item of iterable) {
+    two.push(item);
+    if (two.length > 2) {
+      two.shift();
+    }
+    if (two.length == 2) {
+      yield two;
+    }
   }
-  yield prevState[prevState.length - 1];
+}
+
+function* getInsertions(prevState, rules) {
+  for (const [a, b] of iterateByTwos(prevState)) {
+    yield rules[a + b];
+  }
+}
+
+function* interleave(iterable1, iterable2) {
+  let i1 = iterable1[Symbol.iterator]();
+  let i2 = iterable2[Symbol.iterator]();
+  while (true) {
+    let { value, done } = i1.next();
+    if (done) {
+      return { done };
+    }
+    yield value;
+    yield i2.next().value;
+  }
 }
 
 function apply(prevState, rules) {
-  let state = "";
-  for (const ch of applyGen(prevState, rules)) {
-    state += ch;
+  let insertions = getInsertions(prevState, rules);
+  return interleave(prevState, insertions);
+}
+
+function printState(state) {
+  let s = "";
+  for (const c of state) {
+    s += c;
   }
-  return state;
+  console.log(s);
 }
 
 function solve(initialState, rules) {
   let state = initialState;
   for (let i = 0; i < 10; i++) {
+    printState(state);
     state = apply(state, rules);
   }
 
