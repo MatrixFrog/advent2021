c7d43c736b154ddab64460e8fb70d4ce31d3ce6d
Tyler Breisacher <tbreisacher@drivers.coop>
2022-02-01 18:45:51 -0800

    Done with both parts

--- rs/src/bin/level16/main.rs
+++ rs/src/bin/level16/main.rs
@@ -1,3 +1,5 @@
+use std::iter::Peekable;
+
 fn raw_input() -> &'static str {
   include_str!("input.txt")
 }
@@ -36,21 +38,81 @@ fn input_to_bits() -> Vec<bool> {
   bits
 }
 
-#[derive(Debug)]
 struct Packet {
-  version: u32,
-  type_id: u32,
+  version: u64,
+  type_id: u64,
   contents: PacketContents,
 }
 
-#[derive(Debug)]
+impl std::fmt::Debug for Packet {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    f.debug_struct("Packet")
+      .field("type_id", &self.type_id)
+      .field("contents", &self.contents)
+      .finish()
+  }
+}
 enum PacketContents {
-  Literal(u32),
+  Literal(u64),
   Operator(Vec<Packet>),
 }
 
-fn parse_literal_groups(groups: &[u32]) -> u32 {
-  let mut result: u32 = 0;
+impl std::fmt::Debug for PacketContents {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self {
+      Self::Literal(v) => f.debug_tuple("L").field(v).finish(),
+      Self::Operator(packets) => f.debug_tuple("O").field(packets).finish(),
+    }
+  }
+}
+
+impl Packet {
+  fn evaluate(&self) -> u64 {
+    match &self.contents {
+      PacketContents::Literal(value) => {
+        assert_eq!(self.type_id, 4);
+        return *value;
+      }
+      PacketContents::Operator(subpackets) => {
+        let subpacket_values = subpackets.iter().map(|p| p.evaluate());
+        match self.type_id {
+          0 => subpacket_values.sum(),
+          1 => subpacket_values.product(),
+          2 => subpacket_values.min().unwrap(),
+          3 => subpacket_values.max().unwrap(),
+          5 => {
+            assert_eq!(subpackets.len(), 2);
+            if subpackets[0].evaluate() > subpackets[1].evaluate() {
+              1
+            } else {
+              0
+            }
+          }
+          6 => {
+            assert_eq!(subpackets.len(), 2);
+            if subpackets[0].evaluate() < subpackets[1].evaluate() {
+              1
+            } else {
+              0
+            }
+          }
+          7 => {
+            assert_eq!(subpackets.len(), 2);
+            if subpackets[0].evaluate() == subpackets[1].evaluate() {
+              1
+            } else {
+              0
+            }
+          }
+          _ => panic!("Invalid packet: {:?}", self),
+        }
+      }
+    }
+  }
+}
+
+fn parse_literal_groups(groups: &[u64]) -> u64 {
+  let mut result: u64 = 0;
   for group in groups {
     result <<= 4;
     result += group & 0xF;
@@ -58,7 +120,7 @@ fn parse_literal_groups(groups: &[u32]) -> u32 {
   result
 }
 
-fn parse_int(input: &mut impl Iterator<Item = bool>, bits: u8) -> u32 {
+fn parse_int(input: &mut impl Iterator<Item = bool>, bits: u8) -> u64 {
   let mut result = 0;
   for _ in 0..bits {
     result <<= 1;
@@ -68,8 +130,8 @@ fn parse_int(input: &mut impl Iterator<Item = bool>, bits: u8) -> u32 {
 }
 
 fn parse_packet(input: &mut impl Iterator<Item = bool>) -> Packet {
-  let mut version = parse_int(input, 3);
-  let mut type_id = parse_int(input, 3);
+  let version = parse_int(input, 3);
+  let type_id = parse_int(input, 3);
 
   if type_id == 4 {
     // Literal
@@ -105,13 +167,40 @@ fn parse_packet(input: &mut impl Iterator<Item = bool>) -> Packet {
       for _ in 0..length_of_subpackets_in_bits {
         bits_for_subpackets.push(input.next().unwrap());
       }
-
-      panic!();
+      let subpackets = parse_packets(&mut bits_for_subpackets.into_iter().peekable());
+      Packet {
+        version,
+        type_id,
+        contents: PacketContents::Operator(subpackets),
+      }
     }
   }
 }
 
+fn parse_packets(input: &mut Peekable<impl Iterator<Item = bool>>) -> Vec<Packet> {
+  let mut packets = vec![];
+  while input.peek().is_some() {
+    packets.push(parse_packet(input));
+  }
+  packets
+}
+
+fn calc_version(packet: &Packet) -> u64 {
+  let mut version = packet.version;
+  match &packet.contents {
+    PacketContents::Literal(_) => {}
+    PacketContents::Operator(subpackets) => {
+      for p in subpackets {
+        version += calc_version(&p);
+      }
+    }
+  }
+  version
+}
+
 fn main() {
-  let mut input = input_to_bits().into_iter();
-  parse_packet(&mut input);
+  let mut input = input_to_bits().into_iter().peekable();
+  let packet = parse_packet(&mut input);
+  println!("part 1: {}", calc_version(&packet));
+  println!("part 2: {}", packet.evaluate());
 }
